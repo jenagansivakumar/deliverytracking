dfdf3d237dc3e880d92e02aa99829191fc1d70ee
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2024-11-18 16:06:58 +0100

    created deliveries struct

--- main.go
+++ main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+type deliveries struct {
+	ID           string `json:"id"`
+	CustomerName string `json:"customername"`
+	Address      string `json:"address"`
+}
+
 func main() {
-	fmt.Println("hello world")
+	fmt.Println("Hello world")
 }
