540e8ecae4d1467e769004e4aabadcea9c9b9983
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2024-11-18 16:10:32 +0100

    created DeliveryStatus type

--- main.go
+++ main.go
@@ -2,10 +2,19 @@ package main
 
 import "fmt"
 
+type DeliveryStatus string
+
+const (
+	Pending    DeliveryStatus = "Pending"
+	Dispatched DeliveryStatus = "Dispatched"
+	Delivered  DeliveryStatus = "Delivered"
+)
+
 type deliveries struct {
-	ID           string `json:"id"`
-	CustomerName string `json:"customername"`
-	Address      string `json:"address"`
+	ID           string         `json:"id"`
+	CustomerName string         `json:"customername"`
+	Address      string         `json:"address"`
+	Status       DeliveryStatus `json:"status"`
 }
 
 func main() {
