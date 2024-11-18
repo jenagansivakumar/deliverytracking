3e59093f9e76d6be63e2bb44982fa5420b6d6329
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2024-11-18 18:59:13 +0100

    connecting to db

---
+++ database/db.go
@@ -0,0 +1,22 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+const dbPath string = "./deliveries.db"
+
+func OpenDatabase() (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", dbPath)
+	if err != nil {
+		return nil, fmt.Errorf("error opening db: %w", err)
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, fmt.Errorf("error connecting to database: %w", err)
+
+	}
+	fmt.Println("you are now connected")
+	return db, nil
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/jenagansivakumar/deliverytracking
 
 go 1.23.1
 
-require github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d // indirect
+require (
+	github.com/mattn/go-sqlite3 v1.14.24 // indirect
+	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d // indirect
+)
